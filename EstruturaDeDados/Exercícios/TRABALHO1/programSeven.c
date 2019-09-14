de89b2b928cd80acf278147a7acd24c12a6e49d9
eduardotecnologo <eduardotecnologo@hotmail.com>
2019-09-14 14:29:47 -0300

    Execicio 7

---
+++ EstruturaDeDados/Exercícios/TRABALHO1/programSeven.c
@@ -0,0 +1,20 @@
+// 7. Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos
+// elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor
+// resultante.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int vet_a[10],vet_b[10],vet_c[10],i;
+    for(i = 0;i < 10;i++){
+        printf("Insira o %d elemento do vetor 1: ", (i + 1));
+        scanf("%d", &vet_a[i]);
+    }for(i = 0; i < 10; i++){
+        printf("Insira o %d o elemento do vetor 2: ", (i + 1));
+        scanf("%d", &vet_b[i]);
+    }for(i = 0; i < 10; i++){
+        vet_c[i] = vet_a[i] * vet_b[i];
+        printf("%d O elemento do vetor resultante\n: %d", (i + 1), vet_c[i]);
+    }
+    return 0;
+}
