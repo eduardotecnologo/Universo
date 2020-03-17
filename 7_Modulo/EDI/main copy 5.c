65ca847a6e6117209dcc56da8c351f7889ac2b94
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-17 00:03:48 -0300

    Saída de dados - 6

---
+++ 7_Modulo/EDI/main copy 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i, j;
+  float x;
+  i = 1;
+  j = 2;
+  x = 3;
+  printf("%d", i);
+  printf("%d%.2f", j, x);
+  return 0;
+}
--- 7_Modulo/EDI/main.c
+++ 7_Modulo/EDI/main.c
@@ -3,12 +3,16 @@
 
 int main()
 {
-  int i, j;
-  float x;
-  i = 1;
-  j = 2;
-  x = 3;
-  printf("%d", i);
-  printf("%d%.2f", j, x);
+  // Definição de variáveis
+  float n1, n2, n3, media;
+  // Entrada de dados
+  scanf("%f%f%f", &n1, &n2, &n3);
+  // Operação
+  media = (n1 + n2 + n3) / 3;
+  // Saída de dados
+  printf("%f", n1);
+  printf("%f", n2);
+  printf("%f", n3);
+  printf("%f", media);
   return 0;
 }
