c29ca4fad8041184d641a0496e4623dfaacb41d8
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-17 22:57:26 -0300

    Exercício 01

---
+++ 7_Modulo/EDI/qtdLata.c
@@ -0,0 +1,17 @@
+// Construa um programa que calcule a quantidade de latas de tinta necessárias e o custo para pintar tanques
+//     cilíndricos de combustível,
+//     onde são fornecidos a altura e o raio desse cilindro.Sabendo que :
+//     -a lata de tinta custa R$20, 00
+//     - cada lata contém 5 litros cada litro de tinta pinta 3 metros quadrados.
+//     e que : Área do cilindro = 2 vezes a área da base + circunferencia da base vezes a altura :
+//     e que raio e altura são dados de entrada :
+//     e que : Área do cilindro = 2 vezes a área da base + circunferencia da base vezes a altura :
+//     e que raio e altura são dados de entrada.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+  float altura, raio, areaCilindro, qtdLitros, qtdLatas, custo;
+}
