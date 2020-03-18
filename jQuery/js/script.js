92fadf172001cf44741ed2da4a445c1e2ec267e5
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-17 22:47:00 -0300

    Laços de repetições

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,10 +1,35 @@
-//Estruturas condicioais
-let a = 3;
-if (a % 2 == 1) {
-  alert("O número é impar")
-} else {
-  alert("O núemro é par")
+//Condições - Laços
+let i = 0;
+while (i < 3) {
+  alert(i);
+  i = i + 1;
 }
+// For
+let pula;
+for (pula = 0; pula < 5; pula++) {
+  alert("Pula pirata 5 X")
+}
+
+
+// //Estruturas condicioais
+// let a = 2;
+// let b = 3;
+// let c = "Eduardo Developer";
+
+// if (c % 2 == 1) {
+//   alert("Número ímpar");
+// } else if (c % 2 == 0) {
+//   alert("Valor par")
+// } else {
+//   alert("O valor é inválido!");
+// }
+
+// let a = 3;
+// if (a % 2 == 1) {
+//   alert("O número é impar")
+// } else {
+//   alert("O núemro é par")
+// }
 // Alert
 // alert("Olá Mundo!!!");
 // Variáveis
