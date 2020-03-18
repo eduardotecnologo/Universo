28d8bc6b3f55cfaf36e5219f76b49b75b1bb0d05
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-17 21:03:11 -0300

    Estrutura de dados

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,3 +1,21 @@
+//Estruturas condicioais
+let a = 3;
+if (a % 2 == 1) {
+  alert("O número é impar")
+} else {
+  alert("O núemro é par")
+}
 // Alert
-alert("Olá Mundo!!!");
+// alert("Olá Mundo!!!");
 // Variáveis
+// let msg = "Olá manolo!!"
+// alert(msg)
+// Operações matemática
+// let a = 2;
+// let b = 5;
+
+// let soma = a + b;
+// let sub = a - b;
+// let div = a / b;
+// let mult = a * b;
+// alert(sub)
