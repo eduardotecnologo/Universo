ef87687359905814faaaf60c4c6c8bc0ef85e299
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-30 13:46:41 -0300

    Attr

--- 7_Modulo/POOI/01/src/com/edudeveloper/pooi/HelloWorld.java
+++ 7_Modulo/POOI/01/src/com/edudeveloper/pooi/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.edudeveloper.pooi;
 
 public class HelloWorld {
+	
+	public static String myAttr  = "Attr"; //Atributos - Estados da Classe
+	
 	public static void main(String[] args) {
+		myAttr = "";
+		
 //		String myVariable;
 		
 		// Primitivo
+		
 		int myInt = 0;
 		char myChar = 'C';
 		byte myByte;
@@ -12,6 +18,7 @@ public class HelloWorld {
 		double myDouble = 0.0;
 		
 		// Wrapper
+		
 		Integer myIntW = null;
 		Character myCharW = 'C';
 		Byte myByteW = null;
