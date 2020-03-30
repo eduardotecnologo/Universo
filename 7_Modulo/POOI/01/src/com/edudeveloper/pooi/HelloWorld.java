45bb78574c9e1f0a35a8a1a2a33bb7aa7922f464
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-30 13:41:44 -0300

    Variáveis, tipos de dados

---
+++ 7_Modulo/POOI/01/src/com/edudeveloper/pooi/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.edudeveloper.pooi;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+//		String myVariable;
+		
+		// Primitivo
+		int myInt = 0;
+		char myChar = 'C';
+		byte myByte;
+		float myFloat = 0;
+		double myDouble = 0.0;
+		
+		// Wrapper
+		Integer myIntW = null;
+		Character myCharW = 'C';
+		Byte myByteW = null;
+		Float myFloatW = null;
+		Double myDoubleW = null;
+		String myStrW ="Str";
+		
+		System.out.println("Hello World");
+	}
+
+}
