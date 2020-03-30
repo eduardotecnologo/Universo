040fa2c9f9f9782805fb67bb6bab2ab44eece4cc
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-30 14:01:30 -0300

    Input ouput

---
+++ 7_Modulo/POOI/01/src/com/edudeveloper/pooi/IO.java
@@ -0,0 +1,16 @@
+package com.edudeveloper.pooi;
+
+import java.util.Scanner;
+
+public class IO {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in); //Input
+		System.out.println("Qual o seu nome?");//Output 
+		
+		String name = reader.nextLine(); //Input
+		System.out.println("Seu nome é: " + name);
+		
+		
+	}
+
+}
