b5d0cb8ccadda730b1dc7a74e69afa805327f5b1
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-04-07 14:28:31 -0300

    Classe Retangulo

--- 7_Modulo/POOI/FigurasGeometrica/src/figuras/Retangulo.java
+++ 7_Modulo/POOI/FigurasGeometrica/src/figuras/Retangulo.java
@@ -2,19 +2,43 @@ package figuras;
 
 public class Retangulo implements IObjetoGeometrico{
 	// Atributos
+	
 	protected double largura;
-	pritected double comprimento;
+	protected double comprimento;
 	
 	// Construtores
+	
 	public Retangulo() {
 		this.largura = 5;
 		this.comprimento = 10;
 	}
-	public Retangulo() {
-		
+	public Retangulo(double largura,double comprimento) {
+		this.largura = largura;
+		this.comprimento = comprimento;
 	}
 	//Método Get e Set	
+	
+	public double getLargura() {
+		return largura;
+	}
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
+	public double getComprimento() {
+		return comprimento;
+	}
+	public void setComprimento(double comprimento) {
+		this.comprimento = comprimento;
+	}
+	
+	// Outros Métodos
+	public double calcularArea() {
+		return largura*comprimento;
+	}
+	public double calcularPerimetro() {
+		return 2*largura + 2*comprimento;
+	}
 	public void mostrar() {
-		System.out.println("");
+		System.out.println("Retangulo com largura: "+largura+ " e comprimento: "+comprimento);
 	}
 }
