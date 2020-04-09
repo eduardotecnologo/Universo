fe0b792d1372aec754328d23c6d06b582e2a7d69
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-04-09 17:12:38 -0300

    Class Cilindro

--- 7_Modulo/POOI/FigurasGeometrica/src/figuras/Cilindro.java
+++ 7_Modulo/POOI/FigurasGeometrica/src/figuras/Cilindro.java
@@ -1,5 +1,37 @@
 package figuras;
 
-public class Cilindro {
-
+public class Cilindro extends Circulo{
+	// Atributos
+	private double altura;
+	
+	// Construtores
+	public Cilindro() {
+		super();
+		this.altura = 8;
+	}
+	public Cilindro(double raio, double altura) {
+		super(raio);
+		this.altura = altura;
+	}
+	
+	//Método Get e Set
+	public double  getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	// Outros Métodos
+	@Override
+	public double calcularArea() {
+		return 2*Math.PI*raio*raio+2*Math.PI*raio*altura;
+	}
+	public double calcularVolume() {
+		return 2*Math.PI*altura;
+	}
+	@Override
+	public void mostrar() {
+		System.out.println("Cilindro com raio: "+raio+ " e altura: "+altura);
+	}
 }
