cc395e275d9a0a6845fc62013c4df2fe25e8eabf
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-04-09 17:03:47 -0300

    Class Caixa

--- 7_Modulo/POOI/FigurasGeometrica/src/figuras/Caixa.java
+++ 7_Modulo/POOI/FigurasGeometrica/src/figuras/Caixa.java
@@ -1,5 +1,37 @@
 package figuras;
 
-public class Caixa {
-
+public class Caixa extends Retangulo{
+	// Attributos
+	private double altura;
+	
+	// Construtoras
+	public Caixa() {
+		super();
+		this.altura = 3;
+	}
+	public Caixa(double largura,double comprimento,double altura) {
+		super(largura,comprimento);
+		this.altura = altura;
+	}
+	
+	// Métodos Get e Set
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	// Outros Métodos
+	@Override
+	public double calcularArea() {
+		return 2*(largura*comprimento+comprimento*altura+largura*altura);
+	}
+	public double calcularVolume() {
+		return largura*comprimento*altura;
+	}
+	@Override
+	public void mostrar() {
+		System.out.println("Caixa com largura: "+largura+ ", Comprimento: "+comprimento+ "e altura: "+altura);
+	}
 }
