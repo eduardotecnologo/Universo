fd4496c9c5377408ac697c91aeb699e6ce6a2308
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-04-19 15:57:18 -0300

    Create Class Funcionario

--- 7_Modulo/POO2/Aula01/src/aula/Funcionario.java
+++ 7_Modulo/POO2/Aula01/src/aula/Funcionario.java
@@ -1,7 +1,26 @@
 package aula;
 
 public class Funcionario {
-	public String nome;
+	// Solução 01 - Adiconar atributo cursoSuperior
+	public String name;
 	public String cpf;
+	public String cursoSuperior;
+	
+	Funcionario(String name, String cpf){
+		if(this.validarCpf(cpf)) {
+			this.name = name;
+			this.cpf = cpf;
+		}
+	}
+	
+	
+	public boolean validarCpf(String cpf) {
+		if(cpf.length() == 11) {
+			return true;
+		}
+		return false;
+	}
 
 }
+
+
