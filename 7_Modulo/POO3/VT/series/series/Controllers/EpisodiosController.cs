8a780d9c1d19b7f2f0c261cb9e3d837997274b25
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-06-05 17:43:37 -0300

    Aproveitando tela de login

---
+++ 7_Modulo/POO3/VT/series/series/Controllers/EpisodiosController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using series.DTO;
+
+namespace series.Controllers
+{
+    public class EpisodiosController : Controller
+    {
+        public IActionResult Save(EpisodioDTO episodioTemporario)
+        {
+            return Content("opsa");
+        }
+    }
+}
