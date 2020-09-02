78b6f69fd02a25df2f7ee93a79cfb506ee069a9c
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-09-02 03:02:01 -0300

    Repositório de exercícios

---
+++ 8°-Períudo/Estrutura_I/InfoPessoa/InfoPessoa/InfoPessoa.cpp
@@ -0,0 +1,26 @@
+﻿//1- Faça um algoritmo para ler e em seguida exibir as seguintes informações de uma pessoa:
+// Nome, Idade, Sexo, Peso, Altura, Profissão, Rua, Bairro, Cidade, Estado, CEP, Telefone.
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+    char nome, sexo, profissao, rua, cidade, estado;
+    int idades,cep,telefone;
+    double peso, altura;
+
+    printf("Por favor digite o nome:");
+    scanf_s("%c", &nome);
+    scanf_s("Digite o sexo:");
+    scanf("%c", &sexo);
+
+    //std::cout << "Hello World!\n";
+    system("pause");
+    return 0;
+}
+
